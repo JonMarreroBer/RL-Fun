4699c6c8be4b5b0bc0ec1b5c2903d4473d182516
JonMarreroBer <jamb2001@gmail.com>
2024-11-27 09:00:15 -0600

    2nd commit

--- Atari.py
+++ Atari.py
@@ -11,7 +11,9 @@ env = make_atari_env("SpaceInvadersNoFrameskip-v4",n_envs=4,seed=0)
 env = VecFrameStack(env, n_stack=4)
 
 # Create the agent and train it
-agent = DQN("CnnPolicy", env, verbose=1)
+agent = DQN("CnnPolicy", env, verbose=1,buffer_size=100_000, learning_starts=100_000, train_freq=4, 
+            target_update_interval=1_000, exploration_fraction=0.1, exploration_final_eps=0.01,
+            batch_size=32, learning_rate=1e-4, gradient_steps=1, optimize_memory_usage=False)
 agent.learn(total_timesteps=10_000)
 
 # Create a folder to save videos
@@ -20,8 +22,8 @@ os.makedirs(video_folder, exist_ok=True)
 
 # Wrap environment for video recording
 recording_env = VecVideoRecorder(env, video_folder,
-                                 record_video_trigger=lambda x: x % 5000 == 0,
-                                 video_length=200)  # Record every 5000 steps
+                                 record_video_trigger=lambda x: True,
+                                 video_length=1000) # Record every 5000 steps
 obs = recording_env.reset()
 
 # Record video
